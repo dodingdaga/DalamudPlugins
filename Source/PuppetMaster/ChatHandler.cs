5f456b726ed5b1f6d6b56b400ab1e7c11a49e5f4
DodingDaga <caligula.incitatus@gmail.com>
2025-04-01 16:26:38 +0800

    [PP] 7.2.0.1
    Fix commands not being run on main thread

--- Source/PuppetMaster/ChatHandler.cs
+++ Source/PuppetMaster/ChatHandler.cs
@@ -17,53 +17,50 @@ namespace PuppetMaster
         {
         }
 
-        public static async Task RunMacroAsync(string[] lines, int index)
+        public static async void RunMacroAsync(string[] lines, int index)
         {
-            await Task.Run(() =>
+            Service.semaphore.WaitOne();
+            var reaction = Service.configuration!.Reactions[index];
+            Service.semaphore.Release();
+
+            foreach (var line in lines)
             {
-                Service.semaphore.WaitOne();
-                var reaction = Service.configuration!.Reactions[index];
-                Service.semaphore.Release();
-
-                foreach (var line in lines)
+                var textCommand = Service.FormatCommand(line);
+                if (!string.IsNullOrEmpty(textCommand.Main))
                 {
-                    var textCommand = Service.FormatCommand(line);
-                    if (!string.IsNullOrEmpty(textCommand.Main))
+                    // Process emote
+                    var isEmote = Service.Emotes.Contains(textCommand.Main);
+                    if (isEmote)
                     {
-                        // Process emote
-                        var isEmote = Service.Emotes.Contains(textCommand.Main);
-                        if (isEmote)
-                        {
-                            if ((textCommand.Main == "/sit" || textCommand.Main == "/groundsit" || textCommand.Main == "/lounge") && !reaction.AllowSit)
-                                textCommand.Main = "/no";
-                            if (reaction.MotionOnly)
-                                textCommand.Args = "motion";
-                        }
-
-                        if (!reaction.CommandBlacklist.Contains(textCommand.Main))
-                        {
-                            // Execute command
-                            if (reaction.AllowAllCommands || isEmote || reaction.CommandWhitelist.Contains(textCommand.Main))
-                            {
-                                if (textCommand.Main == "/wait" && float.TryParse(textCommand.Args, out var seconds))
-                                    Thread.Sleep((int)(Math.Clamp(seconds, 0.0, 60.0) * 1000.0));
-                                else
-                                    Chat.Instance.SendMessage($"{textCommand}");
-                            }
-                        }
-#if DEBUG
-                        else
-                        {
-                            Service.ChatGui.Print($"{textCommand.Main} in CommandBlacklist");
-                            return;
-                        }
-#endif
+                        if ((textCommand.Main == "/sit" || textCommand.Main == "/groundsit" || textCommand.Main == "/lounge") && !reaction.AllowSit)
+                            textCommand.Main = "/no";
+                        if (reaction.MotionOnly)
+                            textCommand.Args = "motion";
                     }
+
+                    if (!reaction.CommandBlacklist.Contains(textCommand.Main))
+                    {
+                        // Execute command
+                        if (reaction.AllowAllCommands || isEmote || reaction.CommandWhitelist.Contains(textCommand.Main))
+                        {
+                            if (textCommand.Main == "/wait" && float.TryParse(textCommand.Args, out var seconds))
+                                await Task.Delay((int)(Math.Clamp(seconds, 0.0, 60.0) * 1000.0));
+                            else
+                                Chat.Instance.SendMessage($"{textCommand}");
+                        }
+                    }
+#if DEBUG
+                    else
+                    {
+                        Service.ChatGui.Print($"{textCommand.Main} in CommandBlacklist");
+                        return;
+                    }
+#endif
                 }
-            });
+            }
         }
 
-        public static async void DoCommand(int index, XivChatType type, String message)
+        public static void DoCommand(int index, XivChatType type, String message)
         {
             // Check if part of enabled channels
             if (!Service.configuration!.Reactions[index].EnabledChannels.Contains((int)type)) return;
@@ -93,8 +90,7 @@ namespace PuppetMaster
 
 
             var lines = MyRegex().Split(command.ToString());
-            var task = RunMacroAsync(lines, index);
-            await task;
+            RunMacroAsync(lines, index);
         }
 
         public static void OnChatMessage(XivChatType type, int timestamp, ref SeString sender, ref SeString message, ref bool isHandled)
